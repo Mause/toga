6ba3fb22356a4e7bf29fc0525310cf92681f0c8a
Elliana May <me@mause.me>
2024-11-26 05:26:48 +0000

    add abstract class

--- winforms/tests_backend/hardware/hardware.py
+++ winforms/tests_backend/hardware/hardware.py
@@ -7,3 +7,72 @@ class HardwareProbe(AppProbe):
         super().__init__(app_probe.app)
 
         self.monkeypatch = monkeypatch
+
+
+try:
+    from collections.abc import ABCMeta, abstractmethod
+except AttributeError:
+    ABCMeta = type
+
+    def abstractmethod(a):
+        return a
+
+
+class CameraProbeMixin(metaclass=ABCMeta):
+
+    @abstractmethod
+    def cleanup(self):
+        pass
+
+    @abstractmethod
+    def known_cameras(self):
+        pass
+
+    @abstractmethod
+    def select_other_camera(self):
+        pass
+
+    @abstractmethod
+    def disconnect_cameras(self):
+        pass
+
+    @abstractmethod
+    def reset_permission(self):
+        pass
+
+    @abstractmethod
+    def grant_permission(self):
+        pass
+
+    @abstractmethod
+    def allow_permission(self):
+        pass
+
+    @abstractmethod
+    def reject_permission(self):
+        pass
+
+    @abstractmethod
+    async def wait_for_camera(self, device_count=0):
+        pass
+
+    @abstractmethod
+    @property
+    def shutter_enabled(self):
+        pass
+
+    @abstractmethod
+    async def press_shutter_button(self, photo) -> tuple[str, str, str]:
+        pass
+
+    @abstractmethod
+    async def cancel_photo(self, photo):
+        pass
+
+    @abstractmethod
+    def same_device(self, device, native) -> bool:
+        pass
+
+    @abstractmethod
+    def same_flash_mode(self, expected, actual) -> bool:
+        pass
